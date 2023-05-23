5b28a2a034bd870bdf335bbd3ca08e2274f3b1d7
AzureAD\ManikantaKolli <manikanta.kolli@amphora.net>
2023-05-23 17:43:38 +0530

    Your commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import './App.css'
+import * as Yup from "yup";
+import {Button} from "@mui/material";
 
-function App() {
+interface Todo {
+  id: number;
+  text: string;
+}
+
+const validationSchema = Yup.object().shape({
+  text: Yup.string().required(" Task is required")
+});
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = (data: string) => {
+    const newTodo: Todo = {
+      id: todos.length + 1,
+      text: data
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const deleteTodo = (id: number) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+
+      <Formik
+        initialValues={{ text: "" }}
+        validationSchema={validationSchema}
+        onSubmit={(list, { resetForm }) => {
+          addTodo(list.text);
+          resetForm();
+        }}
+      >
+        <Form>
+          <Field type="text" name="text" placeholder="Enter the task" />
+          <ErrorMessage name="text" component="div" />
+          <button type="submit">Add Task</button>
+        </Form>
+      </Formik>
+
+  {todos.map((item) => (
+    
+    
+        <div key={item.id}>
+          {item.text}{" "}
+          <Button onClick={() => deleteTodo(item.id)}>Delete</Button>
+        </div>
+      ))}
+      
     </div>
   );
-}
+};
 
 export default App;
